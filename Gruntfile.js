2a68ac2853ff3441b846eb0a2de94aeafb2f2310
David Manzer <dmanzer2@gmail.com>
2016-12-21 11:31:46 -0700

    configured grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,67 @@
 module.exports = function (grunt) {
+
+    var log = function (err, stdout, stderr, cb) {
+        if(stdout) {
+            grunt.log.writeln(stdout);
+        }
+        if(stderr) {
+            grunt.log.error(stderr);
+        }
+        cb();
+    };
+
     grunt.initConfig({
         shell: {
-            jekyllBuild: {
-                command: 'jekyll build'
-            }
-        },
-        connect: {
-            server: {
+            jekyllClean: {
+                command: 'jekyll clean',
                 options: {
-                    port: 8080,
-                    base: '_site'
+                    callback: log
+                }
+            },
+            jekyllBuild: {
+                command: 'jekyll build --draft --incremental JEKYLL_ENV=dev',
+                options: {
+                    callback: log
                 }
             }
         },
         watch: {
-          livereload: {
-            files: [
-                '_config.yml',
-                'index.html',
-                '_layouts/**',
-                '_posts/**',
-                '_includes/**',
-            ],
-            tasks: ['shell:jekyllBuild'],
-            options: {
-              livereload: true
-            },
-          },
+            posts:{
+                files:[
+                    '_config.yml',
+                    '*.html',
+                    '*.md',
+                    '_layouts/**',
+                    '_posts/**',
+                    '_drafts/**',
+                    '_includes/**',
+                    'assets/**/*.*',
+                    '_sass/**/*.*',
+                    'css/**/*.*'
+                ],
+                tasks: ['shell:jekyllBuild']
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        '_site/**/*.*'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './_site'
+                }
+            }
         }
+
     });
 
-    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-shell');
-    grunt.registerTask('default', ['shell', 'connect', 'watch'])
-}
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.registerTask('build',['shell:jekyllBuild']);
+    grunt.registerTask('default', ['build', 'browserSync', 'watch']);
+    grunt.registerTask('clean',['shell:jekyllClean']);
+};
